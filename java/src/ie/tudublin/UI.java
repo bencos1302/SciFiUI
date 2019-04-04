a9376374d60989cc7b360bef1280e48e261485a9
Ben Cosgrave <cosgraveben@gmail.com>
2019-04-04 16:00:32 +0100

    Added more ship frame parts

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -45,13 +45,36 @@ public class UI extends PApplet
     {
         background(0);
 
-        // Dashboard
+        // Overhead bar
         noStroke();
-        fill(255);
-        quad(250, 0, 350, 0, 400, 130, 350, 130);
-        quad(850, 0,   950, 0,   850, 130,   800, 130);
-        rect(350, 90, 500, 40);
+        fill(60);
+        quad(250, 0, 320, 0, 400, 130, 350, 130);
+        quad(860, 0, 930, 0, 850, 130, 800, 130);
+        rect(350, 100, 500, 30);
 
+        // Window Sidebars
+        noStroke();
+        quad(0, 350, 120, 400, 100, 420, 0, 380);
+        quad(120, 400, 180, 600, 150, 580, 100, 420);
+        quad(180, 600, 140, 680, 120, 690, 150, 580);
+
+        quad(width - 0, 350, width - 120, 400, width - 100, 420, width - 0, 380);
+        quad(width - 120, 400, width - 180, 600, width - 150, 580, width - 100, 420);
+        quad(width - 180, 600, width - 140, 680, width - 120, 690, width - 150, 580);
+
+        // Main Dashboard
+        rect(0, 700, 100, 20);
+        quad(100, 700, 280, 600, 280, 620, 100, 720);
+        quad(280, 600, 350, 530, 350, 545, 280, 620);
+
+        rect(width - 100, 700, 100, 20);
+        quad(width - 100, 700, width - 280, 600, width - 280, 620, width - 100, 720);
+        quad(width - 280, 600, width - 350, 530, width - 350, 545, width - 280, 620);
+        
+        rect(350, 530, 250, 15);
+        rect(600, 530, 250, 15);
+
+        // Crosshair
         strokeWeight(2);
         stroke(255);
         noFill();
@@ -60,6 +83,7 @@ public class UI extends PApplet
 
         noStroke();
         strokeWeight(1);
+        
         // Modular elements
         
         //radar1.update();
